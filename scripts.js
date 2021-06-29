e4f0b80213df1510a7e1a689930db20973973bff
Fabio Marques <76482044+fabiomarquess@users.noreply.github.com>
2021-06-29 10:27:38 -0300

    Atualizando...

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function insert(num){
+    document.form.textview.value = document.form.textview.value + num;
+}
+function equal(){
+    exp = document.form.textview.value;
+    if(exp){
+       document.form.textview.value = eval(exp);
+}
+}
+function c(){
+    document.form.textview.value = "";
+}
