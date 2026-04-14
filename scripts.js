07d782d4be17409256ae7fa2d0d0bbcd93cb1c66
Fabio Marques <76482044+fabiomarquess@users.noreply.github.com>
2026-04-14 20:36:30 -0300

    Atualizando todo o projeto

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
-function insert(num){
-    document.form.textview.value = document.form.textview.value + num;
-}
-function equal(){
-    exp = document.form.textview.value;
-    if(exp){
-       document.form.textview.value = eval(exp);
-}
-}
-function c(){
-    document.form.textview.value = "";
+const display = document.getElementById("display");
+
+function append(value) {
+  display.value += value;
 }
 
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Erro";
+  }
+}
